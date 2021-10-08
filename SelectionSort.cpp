6340ffc6ff65a8ac222752233be27d72d97b493a
Jaydeep Kumar Silawat <70216818+jayyuuuu@users.noreply.github.com>
2021-10-08 10:42:05 +0530

    Create SelectionSort.cpp

---
+++ SelectionSort.cpp
@@ -0,0 +1,64 @@
+#include<iostream>
+using namespace std;
+void swapping(int &a, int &b) {         //swap the content of a and b
+   int temp;
+   temp = a;
+   a = b;
+   b = temp;
+}
+void display(int *array, int size) {
+   for(int i = 0; i<size; i++)
+      cout << array[i] << " ";
+   cout << endl;
+}
+void selectionSort(int *array, int size) {
+   int i, j, imin;
+   for(i = 0; i<size-1; i++) {
+      imin = i;   //get index of minimum data
+      for(j = i+1; j<size; j++)
+         if(array[j] < array[imin])
+            imin = j;
+         //placing in correct position
+         swap(array[i], array[imin]);
+   }
+}
+int main() {
+   int n;
+   cout << "Enter the number of elements: ";
+   cin >> n;
+   int arr[n];           //create an array with given number of elements
+   cout << "Enter elements:" << endl;
+   for(int i = 0; i<n; i++) {
+      cin >> arr[i];
+   }
+   cout << "Array before Sorting: ";
+   display(arr, n);
+   selectionSort(arr, n);
+   cout << "Array after Sorting: ";
+   display(arr, n);
+}
+
+
+
+
+/*
+
+
+Output
+Enter the number of elements: 6
+Enter elements:
+5 9 7 23 78 20
+Array before Sorting: 5 9 7 23 78 20
+Array after Sorting: 5 7 9 20 23 78
+
+
+*/
+
+
+/*
+
+The complexity of Selection Sort Technique
+Time Complexity: O(n^2)
+
+Space Complexity: O(1)
+*/
